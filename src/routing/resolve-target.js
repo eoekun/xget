654d3fceef0811fa59a162c94665667dfccabbb1
EOEKUN <eoekun@qq.com>
2026-04-13 16:40:41 +0800

    feat: HOME_PAGE_URL to point to Baidu

--- src/routing/resolve-target.js
+++ src/routing/resolve-target.js
@@ -22,7 +22,7 @@ import { normalizeRegistryApiPath } from '../protocols/docker.js';
 import { isFlatpakReferenceFilePath } from '../utils/rewrite.js';
 import { createErrorResponse } from '../utils/security.js';
 
-export const HOME_PAGE_URL = 'https:';
+export const HOME_PAGE_URL = 'https://www.baidu.com/';
 
 /**
  * Creates the canonical homepage redirect response.
